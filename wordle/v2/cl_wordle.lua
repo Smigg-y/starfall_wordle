f58ccfdfb2a08d0870a7cf711a15d2bcb57607e6
Smiggy <70798673+Smigg-y@users.noreply.github.com>
2026-04-30 15:12:25 +0100

    Refactor dirty state management and add timer functions

--- wordle/v2/cl_wordle.lua
+++ wordle/v2/cl_wordle.lua
@@ -39,10 +39,10 @@ function WordleUI:initialize()
     self.player = nil
     self.guesses = 0
 
-    self.dirty = true
+    self:markDirty()
     self.layout = self:performLayout()
 
-    self.grid = WordleGrid:new(self.layout.grid.x, self.layout.grid.y)
+    self.grid = WordleGrid:new(self, self.layout.grid.x, self.layout.grid.y)
 
     self.inputManager = WordleInput.InputManager:new()
     self.keyboard = WordleKeyboard:new(Lang.keyboard, self.inputManager, function(_, letter)
@@ -51,7 +51,29 @@ function WordleUI:initialize()
     self._kbWasActive = true -- clear rt on first render
     self:initializeInputs()
 
-    self.gameTransitionTime = 1 + Animations.bounceDuration + Animations.bounceDelay * (wordLength - 1)
+    self.gameTransitionTime = 0.5 + Animations.bounceDuration + Animations.bounceDelay * (wordLength - 1)
+    self.logoRT = WordleLogo()
+
+    self.timers = {}
+end
+
+function WordleUI:schedule(delay, fn)
+    self.timers[#self.timers + 1] = {
+        time = timer.systime() + delay,
+        fn = fn
+    }
+end
+
+function WordleUI:updateTimers(now)
+    local q = self.timers
+    for i = #q, 1, -1 do
+        if now >= q[i].time then
+            local fn = q[i].fn
+            q[i] = q[#q]
+            q[#q] = nil
+            fn()
+        end
+    end
 end
 
 function WordleUI:markDirty()
@@ -64,7 +86,7 @@ function WordleUI:start()
 end
 
 function WordleUI:_resetState()
-    self.dirty = true
+    self:markDirty()
     self.state = States.waiting
     self.pendingState = nil
     self.player = nil
@@ -77,6 +99,7 @@ function WordleUI:_resetState()
     self.inputManager:setOwner(nil)
     self.inputManager:clearGroup("ui")
     self.inputManager:register("ui", self.playButton)
+    self.timers = {}
 end
 
 function WordleUI:reset()
@@ -86,11 +109,12 @@ function WordleUI:reset()
 end
 
 function WordleUI:onStart(ply)
-    self.dirty = true
+    self:markDirty()
     self.state = States.active
     self.pendingState = nil
     self.endMessage = nil
     self.player = ply
+    self.curPlayerText = l10n.is_playing:format(ply:getName())
     self.guesses = 0
 
     self.grid:reset()
@@ -99,6 +123,7 @@ function WordleUI:onStart(ply)
     self.inputManager:setOwner(ply)
     self.inputManager:clearGroup("ui")
     self.inputManager:register("ui", self.homeButton)
+    self.timers = {}
 end
 
 function WordleUI:onKeyPressed(label)
@@ -109,7 +134,7 @@ function WordleUI:onKeyPressed(label)
     if label == "ENTER" then
         self:submitGuess()
     elseif label == "BKSP" then
-        self.dirty = true
+        self:markDirty()
         self.grid:deleteLastKey()
         Sounds.PlaySound(Sounds.keyBack)
 
@@ -160,12 +185,12 @@ function WordleUI:onGuessResult(feedback, state, answer)
             self.grid:bounceRow()
         end
 
-        timer.simple(self.gameTransitionTime, function()
+        self:schedule(self.gameTransitionTime, function()
             if self.state == States.waiting then
                 return
             end
 
-            self.dirty = true
+            self:markDirty()
 
             if self.pendingState then
                 self.state = self.pendingState
@@ -176,6 +201,7 @@ function WordleUI:onGuessResult(feedback, state, answer)
 
             if state == States.won then
                 self.endMessage = l10n.win_messages[self.guesses]
+                self.solvedInText = l10n.solved_in:format(self.guesses, maxGuesses)
                 if isLocalPlayer then
                     Sounds.PlaySound(Sounds.win)
                 end
@@ -224,7 +250,7 @@ function WordleUI:initializeInputs()
     self.playButton.onHover = function(btn, state)
         if btn.hovered ~= state then
             btn.hovered = state
-            self.dirty = true
+            self:markDirty()
         end
     end
 
@@ -234,15 +260,12 @@ function WordleUI:initializeInputs()
     self.homeButton.onHover = function(btn, state)
         if btn.hovered ~= state then
             btn.hovered = state
-            self.dirty = true
+            self:markDirty()
         end
     end
 
     self.inputManager:register("ui", self.playButton)
 end
-
-render.setFont(Fonts.subtitleMedium)
-local _, subtitleH = render.getTextSize("A")
 
 render.setFont(Fonts.titleMedium)
 local _, titleH = render.getTextSize("A")
@@ -268,18 +291,24 @@ local function drawHomeButton(bounds, hovered)
     render.drawTexturedRect(bounds.x, bounds.y, bounds.w, bounds.h)
 end
 
-local subtextOne = l10n.chances:format(maxGuesses)
-local subtextTwo = l10n.letter_word:format(wordLength)
+render.setFont(Fonts.subtitleMedium)
+local _, subtitleH = render.getTextSize("A")
+local subtitleOne = l10n.chances:format(maxGuesses)
+local subtitleTwo = l10n.letter_word:format(wordLength)
 
 function WordleUI:drawHomeScreen()
-    WordleLogo(self.layout.wordleLogo)
-    drawPlayButton(self.layout.playButton, self.playButton.hovered)
-
-    local bounds = self.layout.subtitle
+    local logoBounds = self.layout.wordleLogo
     render.setColor(Colors.white)
+    render.setRenderTargetTexture(self.logoRT)
+    render.drawTexturedRect(logoBounds.x, logoBounds.y, logoBounds.w, logoBounds.h)
+
+    local subBounds = self.layout.subtitle
     render.setFont(Fonts.subtitleMedium)
-    render.drawText(bounds.x, bounds.y - subtitleH / 2, subtextOne, TEXT_ALIGN.CENTER)
-    render.drawText(bounds.x, bounds.y + subtitleH / 2, subtextTwo, TEXT_ALIGN.CENTER)
+    render.drawText(subBounds.x, subBounds.y - subtitleH / 2, subtitleOne, TEXT_ALIGN.CENTER)
+    render.drawText(subBounds.x, subBounds.y + subtitleH / 2, subtitleTwo, TEXT_ALIGN.CENTER)
+
+    local playBounds, playHovered = self.layout.playButton, self.playButton.hovered
+    drawPlayButton(playBounds, playHovered)
 end
 
 function WordleUI:drawPlayScreen(now)
@@ -288,7 +317,8 @@ function WordleUI:drawPlayScreen(now)
         return
     end
 
-    drawHomeButton(self.layout.homeButton, self.homeButton.hovered)
+    local homeBounds, homeHovered = self.layout.homeButton, self.homeButton.hovered
+    drawHomeButton(homeBounds, homeHovered)
 
     self.grid:update(now)
     self.grid:draw()
@@ -296,12 +326,15 @@ function WordleUI:drawPlayScreen(now)
     local bounds = self.layout.activePlayer
     render.setColor(Colors.white)
     render.setFont(Fonts.small)
-    render.drawText(bounds.x + 48, bounds.y - 16, l10n.is_playing:format(self.player:getName()), TEXT_ALIGN.LEFT)
+    render.drawText(bounds.x + 48, bounds.y - 16, self.curPlayerText, TEXT_ALIGN.LEFT)
 end
 
 function WordleUI:drawResultScreen()
-    drawPlayButton(self.layout.playButton, self.playButton.hovered)
-    drawHomeButton(self.layout.homeButton, self.homeButton.hovered)
+    local playBounds, playHovered = self.layout.playButton, self.playButton.hovered
+    drawPlayButton(playBounds, playHovered)
+
+    local homeBounds, homeHovered = self.layout.homeButton, self.homeButton.hovered
+    drawHomeButton(homeBounds, homeHovered)
 
     local didWin = self.state == States.won
     local col = didWin and Colors.green or Colors.yellow
@@ -314,10 +347,10 @@ function WordleUI:drawResultScreen()
 
     render.setColor(Colors.white)
     render.setFont(Fonts.subtitle)
-    render.drawText(bounds.x, bounds.y + 96, self.endMessage or "", TEXT_ALIGN.CENTER)
+    render.drawText(bounds.x, bounds.y + 96, self.endMessage, TEXT_ALIGN.CENTER)
 
     if didWin then
-        render.drawText(bounds.x, bounds.y + 256, l10n.solved_in:format(self.guesses, maxGuesses), TEXT_ALIGN.CENTER)
+        render.drawText(bounds.x, bounds.y + 256, self.solvedInText, TEXT_ALIGN.CENTER)
     end
 end
 
@@ -327,8 +360,9 @@ function WordleUI:render()
         return
     end
 
+    self:updateTimers(now)
     local grid = self.grid
-    local mainNeedsRedraw = self.dirty or grid._anyAnimating or grid.shakeStart
+    local mainNeedsRedraw = self.dirty or #grid._activeTiles > 0 or grid.shakeStart
     local kbActive = self.state == States.active
     local kbNeedsRedraw = (kbActive and self.keyboard.dirty) or (not kbActive and self._kbWasActive)
 
